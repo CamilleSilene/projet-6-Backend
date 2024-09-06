9563258291320922e9d9e43f8e906ce47f10aacd
CamilleSilene <trillatcam@gmail.com>
2024-09-06 15:32:00 +0200

    ajout du token en .env

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const configRandomToken = process.env.JWT_TOKEN;
+        const decodedToken = jwt.verify(token, configRandomToken);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
